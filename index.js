daf27a67d0ffa00c07511c158912be5f921bfa56
andres2000gb <andres2000gb@gmail.com>
2021-05-17 20:00:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ io.on('connect',  (socket) => {
       const data = await joinGame({jugador: name, partida: room})
       const dataIA = await joinGame({jugador: 'IA', partida: room})
       const { error, user } = addUser({id: socket.id, name, room, orden: data.orden})
+      const { error, user } = addUser({id: socket.id, name:"IA", room, orden: dataIA.orden})
       if(error) return callback(error);
       console.log('Socket ID de join IA: ', user.id)
       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
