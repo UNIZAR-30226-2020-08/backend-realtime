28859b3881ed7689141a59459a3cd6adeb5f7c17
Aaron Ibañez Espes <aaronainzon@gmail.com>
2021-05-19 17:24:58 +0200

    dsaf

--- index.js
+++ index.js
@@ -385,6 +385,15 @@ io.on('connect',  (socket) => {
     }
   });
 
+  socket.on('leavePartidaRP', async (data) => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Las10últimas', text: `${user.name} abandonó la partida.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
+
      /* FORMATO DE DATA
     data = {
     jugador: <username>,
