332dc2e8966976f06980d08d3b141ec1c8cfa701
Aaron Ibañez Espes <aaronainzon@gmail.com>
2021-05-23 00:50:13 +0200

    erh

--- index.js
+++ index.js
@@ -736,6 +736,7 @@ io.on('connect',  (socket) => {
           saveMatches({torneo:data.torneo, matches: dataMatches})
         }
       }
+      io.to(data.torneo).emit('abandonoTorneo', dataPer.jugador);
       //callback()
     }catch(err){
       console.log(err)
